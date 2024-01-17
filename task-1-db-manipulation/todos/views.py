f29b9b5081fa45d582bae94609d3e6eb2f792acf
exkuretrol <33695301+exkuretrol@users.noreply.github.com>
2024-01-17 12:24:40 +0800

    feat: add todolist user filter and auth check

--- task-1-db-manipulation/todos/views.py
+++ task-1-db-manipulation/todos/views.py
@@ -1,30 +1,37 @@
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView
 from django.views.generic.edit import DeleteView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Todo
 from .forms import TodoEditForm
 from django.urls import reverse_lazy
+
 
 class TodoListView(ListView):
     context_object_name = "todos"
     model = Todo
     template_name = "todo_home.html"
 
-class TodoDetailView(DetailView):
-    model = Todo
+    def get_queryset(self):
+        if not self.request.user.is_authenticated:
+            return None
+        return Todo.objects.filter(author=self.request.user).order_by("pk")
 
-class TodoCreateView(CreateView):
+
+class TodoCreateView(LoginRequiredMixin, CreateView):
     model = Todo
     template_name = "todo_create.html"
     fields = ["task", "author"]
     success_url = reverse_lazy("todolist")
 
-class TodoDeleteView(DeleteView):
+
+class TodoDeleteView(LoginRequiredMixin, DeleteView):
     context_object_name = "todo_to_delete"
     model = Todo
     template_name = "todo_delete.html"
     success_url = reverse_lazy("todolist")
 
-class TodoUpdateView(UpdateView):
+
+class TodoUpdateView(LoginRequiredMixin, UpdateView):
     context_object_name = "todo_to_update"
     model = Todo
     template_name = "todo_edit.html"
