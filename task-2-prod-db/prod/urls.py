d3a481b1de33e879ee30272a3896a498d36d076e
exkuretrol <33695301+exkuretrol@users.noreply.github.com>
2024-01-22 02:41:28 +0800

    feat: add search view

--- task-2-prod-db/prod/urls.py
+++ task-2-prod-db/prod/urls.py
@@ -6,11 +6,13 @@ from .views import (
     ProdDetailView,
     ProdDeleteView,
     ProdUpdateView,
+    ProdSearchView
 )
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("prods/", ProdListView.as_view(), name="prod_list"),
+    path("prods/search/", ProdSearchView.as_view(), name="prod_search_result"),
     path("prods/create/", ProdCreateView.as_view(), name="prod_create"),
     path("prods/<int:pk>/", ProdDetailView.as_view(), name="prod_detail"),
     path("prods/<int:pk>/update/", ProdUpdateView.as_view(), name="prod_update"),
--- task-2-prod-db/prod/views.py
+++ task-2-prod-db/prod/views.py
@@ -1,3 +1,6 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.db.models import Q
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from .models import Prod
@@ -14,6 +17,35 @@ class ProdListView(ListView):
     model = Prod
     context_object_name = "prods"
     template_name = "prod_list.html"
+
+
+class ProdSearchView(ListView):
+    model = Prod
+    context_object_name = "prods"
+    template_name = "prod_search_result.html"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        query = self.request.GET.get("q")
+        print(query)
+
+        def query_by_what(query: str):
+            conds = query.split(" ")
+            filters = None
+            filters = Q()
+            for cond in conds:
+                col, value = cond.split(":")
+                if col == "name":
+                    filters &= Q(prod_name__contains=value)
+                elif col == "desc":
+                    filters &= Q(prod_desc__contains=value)
+                elif col == "type":
+                    filters &= Q(prod_type__contains=value)
+                elif col == "status":
+                    filters &= Q(prod_status__contains=value)
+            return filters
+
+        prods_list = Prod.objects.filter(query_by_what(query))
+        return prods_list
 
 
 class ProdCreateView(CreateView):
