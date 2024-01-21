577e550334a645dbf10d85f2c0c3211a9276d534
exkuretrol <33695301+exkuretrol@users.noreply.github.com>
2024-01-21 12:08:14 +0800

    feat: add article update, detail, delete view

--- task-2-prod-db/prod/views.py
+++ task-2-prod-db/prod/views.py
@@ -1,13 +1,14 @@
-from django.views.generic import ListView
-from django.views.generic.edit import CreateView
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from .models import Prod
+from django.urls import reverse_lazy
 
 
 class ProdDetailView(DetailView):
     model = Prod
     context_object_name = "prod"
     template_name = "prod_detail.html"
+
 
 class ProdListView(ListView):
     model = Prod
@@ -26,3 +27,23 @@ class ProdCreateView(CreateView):
         "prod_quantity",
         "prod_status",
     ]
+
+
+class ProdUpdateView(UpdateView):
+    model = Prod
+    fields = [
+        "prod_name",
+        "prod_desc",
+        "prod_img",
+        "prod_type",
+        "prod_quantity",
+        "prod_status",
+    ]
+    template_name = "prod_update.html"
+
+
+class ProdDeleteView(DeleteView):
+    model = Prod
+    template_name = "prod_delete.html"
+    context_object_name = "prod"
+    success_url = reverse_lazy("prod_list")
