12de8e2c9332b6844206683f2f1c2eef75dd86cd
exkuretrol <33695301+exkuretrol@users.noreply.github.com>
2024-01-24 12:29:41 +0800

    feat: custom login / signup forms

--- task-2-prod-db/accounts/forms.py
+++ task-2-prod-db/accounts/forms.py
@@ -1,8 +1,16 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
+from typing import Any
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    UserChangeForm,
+)
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import password_validation
+from django.contrib.auth.forms import AuthenticationForm, UsernameField
 from .models import CustomUser
+from django import forms
 
 
-class CustomUserCreationForm(UserCreationForm):
+class CommonUserInfo(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = (
@@ -10,6 +18,55 @@ class CustomUserCreationForm(UserCreationForm):
             "email",
             "age",
         )
+
+        widgets = {
+            "username": forms.TextInput(attrs={"class": "input-normal"}),
+            "email": forms.EmailInput(attrs={"class": "input-normal"}),
+            "age": forms.NumberInput(attrs={"class": "input-normal"}),
+        }
+
+        labels = {
+            "username": "Username",
+            "email": "Email",
+            "age": "Age",
+        }
+
+
+class CustomUserCreationForm(UserCreationForm):
+    password1 = forms.CharField(
+        label="Password",
+        strip=False,
+        widget=forms.PasswordInput(
+            attrs={"autocomplete": "new-password", "class": "input-normal"}
+        ),
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+
+    password2 = forms.CharField(
+        label="Password Confirmation",
+        widget=forms.PasswordInput(
+            attrs={"autocomplete": "new-password", "class": "input-normal"}
+        ),
+        strip=False,
+        help_text=_("Enter the same password as before, for verification."),
+    )
+
+    class Meta(CommonUserInfo.Meta):
+        pass
+
+
+class CustomUserAuthenticationForm(AuthenticationForm):
+    # def __init__(self, request: Any = ..., *args: Any, **kwargs: Any) -> None:
+    #     super().__init__(request, *args, **kwargs)
+    #     self.fields["username"].widget = forms.TextInput(attrs={"autofocus": True, "class": "input-normal"})
+
+    username = UsernameField(widget=forms.TextInput(attrs={"autofocus": True, "class": "input-normal"}))
+    password = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={"class":"input-normal", "autocomplete": "current-password"}),
+    )
+
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
--- task-2-prod-db/accounts/urls.py
+++ task-2-prod-db/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, CustomLoginView
 
 urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup")
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path("login/", CustomLoginView.as_view(), name="login"),
 ]
