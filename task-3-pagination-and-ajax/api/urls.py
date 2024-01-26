49b0a3714203a15f3b4770c3f7ceeb6222375244
exkuretrol <33695301+exkuretrol@users.noreply.github.com>
2024-01-26 22:02:22 +0800

    feat: add get_prods function

--- task-3-pagination-and-ajax/api/urls.py
+++ task-3-pagination-and-ajax/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from django.views.generic import TemplateView
-from .views import get_data
+from .views import get_data, get_prods, ProdListView
 
 urlpatterns = [
-    path("goods/ajax_get_category/9x9shop", get_data,name="ajax_get_category"),
-    path("ajax/", TemplateView.as_view(template_name="prod_ajax.html"), name="ajax_test")
+    path("goods/ajax_get_category/9x9shop", get_data, name="ajax_get_category"),
+    path("goods/ajax_get_prods/9x9shop", get_prods, name="ajax_get_prods"),
+    path(
+        "ajax/", ProdListView.as_view(), name="ajax_test"
+    ),
 ]
--- task-3-pagination-and-ajax/api/views.py
+++ task-3-pagination-and-ajax/api/views.py
@@ -1,8 +1,28 @@
-from django.http import JsonResponse
+from typing import Any
+from django.http import HttpResponse, JsonResponse, HttpRequest
+from django.views.generic import ListView
+from django.views.generic.edit import FormMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from prod.forms import QueryForm
 from prod.models import Prod
 from prod.utils import prod_query
 
+
+class ProdListView(LoginRequiredMixin, FormMixin, ListView):
+    model = Prod
+    form_class = QueryForm
+    template_name = "prod_ajax.html"
+    context_object_name = "prods"
+    # def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+    #     return super().get(request, *args, **kwargs)
+
+
 def get_data(request):
+    return JsonResponse({"message": "hello"})
+
+
+def get_prods(request: HttpRequest):
+    query_str = request.GET.get("query", "")
     object_list = Prod.objects.all()
-    data = list(object_list.filter(prod_query("type:t1")).values())
+    data = list(object_list.filter(prod_query(query_str)).values())
     return JsonResponse({"data": data})
